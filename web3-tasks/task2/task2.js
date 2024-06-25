c399a6ebc230cddee29d2e19fc8b607afbaf5903
Himanish kochhar <himanish.kochhar@gmail.com>
2024-06-25 10:55:06 +0530

    task2 sha256 added code

---
+++ web3-tasks/task2/task2.js
@@ -0,0 +1,23 @@
+let crypto = require('crypto')
+/*
+  prefixHash will use to identify the hash for the numbers strating from Zero. 
+  and in which hash has prefix like "00000" 
+  here the intial string passed in function would be like 'himanish' + '00000' 
+  we are using brutefoce method to fnd the hash that starting with '00000'
+*/
+
+const prefixHash = prefix => {
+  let inpt = 0
+
+  while (true) {
+    let stringInput = 'himanish' + inpt.toString()
+    let hash = crypto.createHash('sha256').update(stringInput).digest('hex')
+    if (hash.startsWith(prefix)) {
+      return { input: stringInput, hash: hash }
+    }
+    inpt++
+  }
+}
+
+const result = prefixHash('00000')
+console.log(`Input string : ${result.input} ,  Hash : ${result.hash}`)
